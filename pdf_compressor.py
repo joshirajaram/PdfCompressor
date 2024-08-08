fef23077c79b77ccd4dab7a88b5898b99959e366
Rajaram Joshi <rajaram8799@gmail.com>
2024-08-08 22:35:00 +0530

    feat: Load configs from yaml

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,40 +1,56 @@
 from pypdf import PdfWriter
 import os
 import time
+import yaml
 
-env_filepath = os.getenv("SRC_ANSWER_SHEETS_FILE_PATH")
-print("SRC File path:", env_filepath)
-start_time = time.time()
-answer_sheets = []
-original_sizes = {}
-compressed_sizes = {}
-for (root, dirs, files) in os.walk(env_filepath):
-    answer_sheets = files
-    
-print("answer sheets: ", answer_sheets)
+def load_config(file_path):
+    """Load configuration from a YAML file."""
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-for file in answer_sheets:
-    original_sizes[file] = os.path.getsize(os.path.join(env_filepath, file))
-    
-os.mkdir(env_filepath + "/compressed_pdfs")
-    
-for file in answer_sheets:
-    writer = PdfWriter(clone_from=os.path.join(env_filepath, file))
-
-    for page in writer.pages:
-        for img in page.images:
-            img.replace(img.image, quality=10)
-
-    with open(env_filepath + "/compressed_pdfs/" + file, "wb") as f:
-        writer.write(f)
+def main(config):
+    src_filepath = config["SRC_ANSWER_SHEETS_FILE_PATH"]
+    print("SRC File path:", src_filepath)
+    start_time = time.time()
+    answer_sheets = []
+    original_sizes = {}
+    compressed_sizes = {}
+    for (root, dirs, files) in os.walk(src_filepath):
+        answer_sheets = files
         
-end_time = time.time()
+    print("discovered answer sheets:", answer_sheets)
 
-print("Total time elapsed: ", end_time - start_time)
+    for file in answer_sheets:
+        original_sizes[file] = os.path.getsize(os.path.join(src_filepath, file))
+        
+    os.mkdir(src_filepath + "/compressed_pdfs")
+        
+    for file in answer_sheets:
+        print("Compressing file:", file)
+        writer = PdfWriter(clone_from=os.path.join(src_filepath, file))
 
-for file in answer_sheets:
-    compressed_sizes[file] = os.path.getsize(os.path.join(env_filepath, "compressed_pdfs", file))
-    
-for file in answer_sheets:
-    print("File name: ", file,  "Original size: ", original_sizes[file], 
-          "Compressed size: ", compressed_sizes[file], "Percentage reduction: ", (original_sizes[file] - compressed_sizes[file])*100/original_sizes[file]//1)
+        for page in writer.pages:
+            for img in page.images:
+                img.replace(img.image, quality=config["IMAGE_QUALITY_REDUCTION_FACTOR"])
+
+        with open(src_filepath + "/compressed_pdfs/" + file, "wb") as f:
+            writer.write(f)
+            
+    end_time = time.time()
+
+    print("Total time elapsed: ", end_time - start_time)
+
+    for file in answer_sheets:
+        compressed_sizes[file] = os.path.getsize(os.path.join(src_filepath, "compressed_pdfs", file))
+        
+    for file in answer_sheets:
+        print("File name:", file,  "Original size:", original_sizes[file], 
+            "Compressed size:", compressed_sizes[file], "Percentage reduction:", (original_sizes[file] - compressed_sizes[file])*100/original_sizes[file]//1)
+        
+if __name__ == '__main__':
+    config = load_config('./configs.yaml')
+    try:
+        main(config)
+    except:
+        print("Exception occurred. Please check stacktrace.")
